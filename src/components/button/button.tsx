f45b14fe920ebf7ed6e97a6f2080420d5fcb5c57
mvindasr <mvindasr@ucenfotec.ac.cr>
2024-08-16 08:49:26 -0600

    add class variance authority and mdx for documentation

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,24 +1,15 @@
 import { ComponentProps } from 'react';
-import styles from './button.module.css';
-import clsx from 'clsx';
+import { variants, type ButtonVariants } from './button-variants';
 
-export type ButtonProps = ComponentProps<'button'> & {
-  variant?: 'primary' | 'secondary' | 'destructive';
-  size?: 'small' | 'medium' | 'large';
-};
+export type ButtonProps = ComponentProps<'button'> &
+  ButtonVariants & {
+    size?: 'small' | 'medium' | 'large';
+  };
 export const Button = ({
   variant = 'primary',
   size = 'medium',
   className,
   ...props
 }: ButtonProps) => {
-  let classes = clsx(
-    'bg-indigo-500 hover:bg-indigo-400',
-    styles.button,
-    styles[variant],
-    styles[size],
-    className,
-  );
-
-  return <button {...props} className={classes} />;
+  return <button {...props} className={variants({ variant, size })} />;
 };
