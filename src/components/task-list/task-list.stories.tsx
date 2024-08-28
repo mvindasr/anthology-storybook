31bda3cab68b57348a5b519e577549b4ee241a81
mvindasr <mvindasr@ucenfotec.ac.cr>
2024-08-28 08:36:32 -0600

    add decorators and loaders example to task list

--- src/components/task-list/task-list.stories.tsx
+++ src/components/task-list/task-list.stories.tsx
@@ -1,10 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TaskList } from './task-list';
+import { Story } from '@storybook/blocks';
+import { TaskListProvider } from './task-list-context';
 
 const meta = {
   title: 'Components/TaskList',
   component: TaskList,
+  loaders: [
+    async () => {
+      const tasks = await fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+        res.json(),
+      );
+      return { tasks };
+    },
+  ],
+  decorators: [
+    (Story, context) => {
+      return (
+        <TaskListProvider tasks={context.loaded.tasks}>
+          <Story {...context} />
+        </TaskListProvider>
+      );
+    },
+  ],
 } as Meta<typeof TaskList>;
 
 export default meta;
 type Story = StoryObj<typeof TaskList>;
+
+export const Default: Story = {
+  args: {},
+};
